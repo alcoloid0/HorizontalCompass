3ce1baa2ae7c42501ffdf5e5edc416d2f209023f
alcoloid0 <171401998+alcoloid0@users.noreply.github.com>
2024-06-10 20:30:58 +0300

    refactor(src): remove classes associated with EssentialsX

--- src/main/java/com/github/alcoloid0/horizontalcompass/hook/ProtocolLibWrapper.java
+++ src/main/java/com/github/alcoloid0/horizontalcompass/hook/ProtocolLibWrapper.java
@@ -20,26 +20,16 @@ package com.github.alcoloid0.horizontalcompass.hook;
 import com.comphenix.protocol.ProtocolLibrary;
 import com.comphenix.protocol.ProtocolManager;
 import com.comphenix.protocol.events.PacketListener;
-import org.bukkit.plugin.Plugin;
 import org.jetbrains.annotations.NotNull;
 
-public final class ProtocolLibHook implements Hook {
-    private final Plugin plugin;
-    private final Plugin protocolLibPlugin;
-    private final ProtocolManager protocolManager;
+public final class ProtocolLibWrapper {
+    private static final ProtocolManager manager = ProtocolLibrary.getProtocolManager();
 
-    public ProtocolLibHook(@NotNull Plugin protocolLib, @NotNull Plugin plugin) {
-        this.plugin = plugin;
-        this.protocolLibPlugin = protocolLib;
-        this.protocolManager = ProtocolLibrary.getProtocolManager();
+    private ProtocolLibWrapper() throws IllegalAccessException {
+        throw new IllegalAccessException();
     }
 
-    public void addPacketListener(@NotNull Object packetListener) {
-        this.protocolManager.addPacketListener((PacketListener) packetListener);
-    }
-
-    @Override
-    public @NotNull Plugin getPlugin() {
-        return this.protocolLibPlugin;
+    public static void addPacketListener(@NotNull Object packetListener) {
+        manager.addPacketListener((PacketListener) packetListener);
     }
 }
